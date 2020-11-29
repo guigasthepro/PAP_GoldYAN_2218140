c283abdc1685cf5767e4083d1196bd800938f272
Guilherme Firmino Gomes Borba Simão <2218140@alunos.escoladigital.com>
2020-11-29 18:16:11 +0000

    Introduction Of My Project

---
+++ BlazorAdmin2/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorAdmin2
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult> Login([FromForm] string password)
+        {
+            if(password == "password")
+            {
+                List<Claim> claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Role, "admin")
+                };
+
+                ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, "auth");
+                ClaimsPrincipal claimsprincipal = new ClaimsPrincipal(claimsIdentity);
+                await HttpContext.SignInAsync(claimsprincipal);
+
+            }
+            return Redirect("/");
+        }
+    }
+}
