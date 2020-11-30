1fd0dd1b75a61136215c36a18b0ae8024be6a890
Guilherme Firmino Gomes Borba Simão <2218140@alunos.escoladigital.com>
2020-11-30 20:46:08 +0000

    Trying to implement login

---
+++ BlazorAdmin2/Components/ServiceComponent.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using RestSharp;
+using RestSharp.Serializers;
+
+namespace BlazorAdmin2.Components
+{
+    public interface IServiceComponent
+    {
+        IRestResponse ResponseJson(string url, object requestBody, Dictionary<string, string> requesHeader, List<Parameter> requestParameter, Method method);
+
+        IRestResponse ResponseJsonAuth(string url, object requestBody, Dictionary<string, string> requesHeader, Method method);
+
+    }
+    public class ServiceComponent : IServiceComponent
+    {
+        public IRestResponse ResponseJson(string url, object requestBody, Dictionary<string, string> requesHeader, List<Parameter> requestParameter,
+            Method method)
+        {
+            var client = new RestClient(url);
+            var request = new RestRequest(method)
+            {
+                RequestFormat = DataFormat.Json,
+                JsonSerializer = new CamelCaseSerializer()
+            };
+            if (requesHeader != null)
+            {
+                foreach (var item in requesHeader)
+                {
+                    request.AddHeader(item.Key, item.Value);
+                }
+            }
+
+            if (requestParameter != null)
+            {
+                foreach (var item in requestParameter)
+                {
+                    request.AddParameter(item);
+                }
+
+            }
+
+            if (requestBody != null)
+            {
+                request.AddJsonBody(requestBody);
+            }
+
+            IRestResponse response = client.Execute(request);
+            return response;
+
+
+        }
+
+        public IRestResponse ResponseJsonAuth(string url, object requestBody, Dictionary<string, string> requesHeader, Method method)
+        {
+            var client = new RestClient(url);
+            var request = new RestRequest(method)
+            {
+                RequestFormat = DataFormat.Json,
+                JsonSerializer = new CamelCaseSerializer()
+            };
+            if (requesHeader != null)
+            {
+                foreach (var item in requesHeader)
+                {
+                    request.AddHeader(item.Key, item.Value);
+                }
+            }
+
+            if (requestBody != null)
+            {
+                request.AddJsonBody(requestBody);
+            }
+
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
+
+        public class CamelCaseSerializer : ISerializer
+        {
+            public string ContentType { get; set; }
+
+            public CamelCaseSerializer()
+            {
+                ContentType = "application/json";
+            }
+
+            public string Serialize(object obj)
+            {
+                var camelCaseSetting = new JsonSerializerSettings()
+                {
+                    ContractResolver = new CamelCasePropertyNamesContractResolver()
+                };
+                string json = JsonConvert.SerializeObject(obj, camelCaseSetting);
+                return json;
+            }
+
+
+        }
+
+    }
+}
--- BlazorAdmin2/Startup.cs
+++ BlazorAdmin2/Startup.cs
@@ -1,8 +1,12 @@
+using System.Text;
+using BlazorAdmin2.Components;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 
 namespace BlazorAdmin2
 {
@@ -22,6 +26,22 @@ namespace BlazorAdmin2
             services.AddRazorPages();
             services.AddServerSideBlazor();
             //services.AddSingleton<WeatherForecastService>();
+            services.AddScoped<ServiceComponent>();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(option =>
+                {
+                    option.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration["Jwt:Issuer"],
+                        ValidAudience = Configuration["Jwt:Issuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                    };
+                });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,7 +69,7 @@ namespace BlazorAdmin2
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                //endpoints.MapDefaultControllerRoute();
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
             });
